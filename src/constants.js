c0dd6bc8dac0f5ce0725a95006e43a5ee9cf0473
adityasuman2025 <adityasuman2025@gmail.com>
2020-12-08 09:45:52 +0530

    landing screen UI done

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@
 export const GRADIENT_PURPLE_LIGHT = "#35176d";
 export const GRADIENT_PURPLE_DARK = "#22045a";
 
-export const GRADIENT_GREY_LIGHT = "lightgrey";
-export const GRADIENT_GREY_DARK = "#e2e2e2";
+export const GRADIENT_GREY_LIGHT = "lightgrey"; //"#6c519c";
+export const GRADIENT_GREY_DARK = "#e2e2e2"; //"#3f2177";
